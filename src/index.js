233fdc7626b0d1759c47e8a0fd7185ba1e55b7d6
SHARDUL SHIVAM <55155099+sharduls27@users.noreply.github.com>
2023-04-24 13:05:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ ReactDOM.render(
 var h1 = document.createElement("h1");
 h1.innerHTML = "hello world!!";
 document.getElementById("root").appendChild(h1);
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Noodles</li>
+      <li>Jamon</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
